f63fed1790d60a0c1848f7435a6af56acded01b7
fworg64 <fworg64@gmail.com>
2017-09-14 21:42:54 -0500

    added a few test cases for localization lib

--- src/localization/src/tests/localizer_test_cycle1.cpp
+++ src/localization/src/tests/localizer_test_cycle1.cpp
@@ -17,9 +17,53 @@ TEST(MotionTracking, GoesForward)
   ON_CALL(frvesc, getLinearVelocity()).WillByDefault(Return(.5));
   ON_CALL(brvesc, getLinearVelocity()).WillByDefault(Return(.5));
   ON_CALL(blvesc, getLinearVelocity()).WillByDefault(Return(.5));
+  EXPECT_CALL(flvesc, getLinearVelocity());
+  EXPECT_CALL(frvesc, getLinearVelocity());
+  EXPECT_CALL(brvesc, getLinearVelocity());
+  EXPECT_CALL(blvesc, getLinearVelocity());
 
   loki.updateStateVector(.01);  // 10 ms, 100Hz update
-  EXPECT_TRUE(loki.stateVector.xPos >= .0049);
+  EXPECT_TRUE(loki.stateVector.xPos >= .0049 && loki.stateVector.xPos <= .0051);
+  EXPECT_TRUE(loki.stateVector.yPos >= -.0001 && loki.stateVector.yPos <= .0001);
+  EXPECT_TRUE(loki.stateVector.theta >= -.0001 && loki.stateVector.theta <= .0001);
+}
+
+TEST(MotionTracking, ForwardLeft)
+{
+  MockVescAccess flvesc, frvesc, brvesc, blvesc;
+  Localizer loki(&flvesc, &frvesc, &brvesc, &blvesc);
+  ON_CALL(flvesc, getLinearVelocity()).WillByDefault(Return(.3));
+  ON_CALL(frvesc, getLinearVelocity()).WillByDefault(Return(.5));
+  ON_CALL(brvesc, getLinearVelocity()).WillByDefault(Return(.5));
+  ON_CALL(blvesc, getLinearVelocity()).WillByDefault(Return(.3));
+  EXPECT_CALL(flvesc, getLinearVelocity());
+  EXPECT_CALL(frvesc, getLinearVelocity());
+  EXPECT_CALL(brvesc, getLinearVelocity());
+  EXPECT_CALL(blvesc, getLinearVelocity());
+
+  loki.updateStateVector(.01);  // 10 ms, 100Hz update
+  EXPECT_TRUE(loki.stateVector.xPos >= .0039 && loki.stateVector.xPos <= .0041);
+  EXPECT_TRUE(loki.stateVector.yPos >= .0000079 && loki.stateVector.yPos <= .0000081);
+  EXPECT_TRUE(loki.stateVector.theta >= .0039 && loki.stateVector.theta <= .0041);
+}
+
+TEST(MotionTracking, ForwardRight)
+{
+  MockVescAccess flvesc, frvesc, brvesc, blvesc;
+  Localizer loki(&flvesc, &frvesc, &brvesc, &blvesc);
+  ON_CALL(flvesc, getLinearVelocity()).WillByDefault(Return(.5));
+  ON_CALL(frvesc, getLinearVelocity()).WillByDefault(Return(.3));
+  ON_CALL(brvesc, getLinearVelocity()).WillByDefault(Return(.3));
+  ON_CALL(blvesc, getLinearVelocity()).WillByDefault(Return(.5));
+  EXPECT_CALL(flvesc, getLinearVelocity());
+  EXPECT_CALL(frvesc, getLinearVelocity());
+  EXPECT_CALL(brvesc, getLinearVelocity());
+  EXPECT_CALL(blvesc, getLinearVelocity());
+
+  loki.updateStateVector(.01);  // 10 ms, 100Hz update
+  EXPECT_TRUE(loki.stateVector.xPos >= .0039 && loki.stateVector.xPos <= .0041);
+  EXPECT_TRUE(loki.stateVector.yPos >= -.0000081 && loki.stateVector.yPos <= -.0000079);
+  EXPECT_TRUE(loki.stateVector.theta >= -.0041 && loki.stateVector.theta <= -.0039);
 }
 
 // Run all the tests that were declared with TEST()
