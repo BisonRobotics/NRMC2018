bf602b9fcd0469d5d8c57a4e6bb8061db77211ed
Marc Olberding <dashneptune@gmail.com>
2018-01-09 19:23:08 -0600

    made requirements and started on implementation

---
+++ src/hardware_layer/can_sensors/include/lp_research/lpresearchimu.h
@@ -0,0 +1,12 @@
+#ifndef __LP_RESEARCH_IMU__
+#define __LP_RESEARCH_IMU__
+
+
+class LpResearchImu :{
+
+};
+
+
+
+
+#endif
---
+++ src/hardware_layer/can_sensors/src/lp_research/lpresearchimu.cpp
@@ -0,0 +1,6 @@
+#include "lpresearchimu.h"
+
+
+
+LpResearchImu::LpResearchImu (){
+}
