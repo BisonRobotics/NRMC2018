74ccc2dd1dc68a16a78084fa17f12fc648975650
Marc Olberding <dashneptune@gmail.com>
2018-01-13 03:30:39 -0600

    Added extra check in integration test to check readable sensor status

--- src/hardware_layer/sensor_access/tests/lpresearchtest.cpp
+++ src/hardware_layer/sensor_access/tests/lpresearchtest.cpp
@@ -6,7 +6,11 @@ int main (int argc, char **argv){
    LpResearchImu *lp = new LpResearchImu ("imu");
   ros::NodeHandle n;
   while (ros::ok ()){
-    ROS_INFO ("%f %f %f ", lp->getX(), lp->getY(), lp->getAlpha() );
+      if (lp->receiveData() == ReadableSensors::ReadStatus::READ_SUCCESS) {
+          ROS_INFO ("%f %f %f ", lp->getX(), lp->getY(), lp->getAlpha());
+      } else {
+          ROS_INFO ("No Data received");
+      }
     ros::spinOnce ();
   }
 }
