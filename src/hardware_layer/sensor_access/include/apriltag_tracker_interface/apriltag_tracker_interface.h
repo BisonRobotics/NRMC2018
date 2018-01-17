1568b8d3d99c26700f94e345bf5a26afbac09856
jacobhuesman <jacob.huesman@ndsu.edu>
2018-01-16 20:11:23 -0600

    Added position interface

---
+++ src/hardware_layer/sensor_access/include/apriltag_tracker_interface/apriltag_tracker_interface.h
@@ -0,0 +1,29 @@
+#ifndef PROJECT_APRILTAG_TRACKER_INTERFACE_H
+#define PROJECT_APRILTAG_TRACKER_INTERFACE_H
+
+#include <sensor_access/pos_sensor_interface.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_ros/transform_listener.h>
+
+class AprilTagTrackerInterface : public PosSensorInterface
+{
+public:
+  AprilTagTrackerInterface();
+  ~AprilTagTrackerInterface();
+  double getX() override;
+  double getY() override;
+  double getTheta() override ;
+  ReadableSensors::ReadStatus receiveData() override;
+
+private:
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener *tfListener;
+
+  double x;
+  double y;
+  double theta;
+  tf2::Stamped<tf2::Transform> map_to_position_estimate_tf;
+};
+
+
+#endif //PROJECT_APRILTAG_TRACKER_INTERFACE_H
