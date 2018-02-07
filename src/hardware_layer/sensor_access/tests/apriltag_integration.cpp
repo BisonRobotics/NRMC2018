ad9964b56078841e64d13ef13161fe67d741465e
Marc Olberding <dashneptune@gmail.com>
2018-02-06 19:21:22 -0600

    added in integration file

--- src/hardware_layer/sensor_access/tests/apriltag_integration.cpp
+++ src/hardware_layer/sensor_access/tests/apriltag_integration.cpp
@@ -1,10 +1,11 @@
 #include "apriltag_tracker_interface/apriltag_tracker_interface.h"
-#include "ros/ros.h"
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "imu_listener");
-  AprilTagTrackerInterface ap ("pose_estimate", .1);
+  AprilTagTrackerInterface ap ("pose_estimate", .01);
+  ros::Rate loop_rate (50);
   ros::NodeHandle n;
   while (ros::ok())
   {
@@ -16,6 +17,7 @@ int main(int argc, char **argv)
     {
       ROS_INFO("No Data received");
     }
+    loop_rate.sleep ();
     ros::spinOnce();
   }
 }
