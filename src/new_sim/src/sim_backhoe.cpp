44d14f10c64878137eefb3bea2512155d092cfc1
fworg64 <fworg64@gmail.com>
2018-02-21 21:15:18 -0600

    added and built outrigger simulator, commented out non-building stuff in for backhoe controller

--- src/new_sim/src/sim_backhoe.cpp
+++ src/new_sim/src/sim_backhoe.cpp
@@ -9,11 +9,21 @@ SimBackhoe::SimBackhoe(double shouldlerTheta, double wristTheta)
     wr = new SimVesc(16, 0, 1.0);
 }
 
-SimBackhoe::update(double dt)
+void SimBackhoe::update(double dt)
 {
     sh->update(dt);
     wr->update(dt);
 
     shTh += sh->getLinearVelocity() * dt;
     wrTh += wr->getLinearVelocity() * dt;
+}
+
+double SimBackhoe::getShTheta()
+{
+  return shTh;
+}
+
+double SimBackhoe::getWrTheta()
+{
+  return wrTh;
 }
---
+++ src/new_sim/src/sim_outriggers.cpp
@@ -0,0 +1,51 @@
+#include "sim_robot/sim_outriggers.h"
+
+SimOutriggers::SimOutriggers(double initialPosL, double initialPosR)
+{
+  l = new SimVesc(16, 0, 1.0);
+  r = new SimVesc(16, 0, 1.0);
+
+  posL = initialPosL;
+  posR = initialPosR;
+}
+
+void SimOutriggers::update(double dt)
+{
+  l->update(dt);
+  r->update(dt);
+
+  posL += l->getLinearVelocity() *dt;
+  posR += r->getLinearVelocity() *dt;
+
+  if (posL < 0) 
+  {
+     posL =0;
+     l->setLinearVelocity(0);
+  }
+  else if (posL > .1778) 
+  {
+       posL = .1778;
+       l->setLinearVelocity(0);
+  }
+
+  if (posR < 0) 
+  {
+     posR =0;
+     r->setLinearVelocity(0);
+  }
+  else if (posR > .1778)
+  {
+      posR = .1778;
+      r->setLinearVelocity(0);
+  }
+}
+
+double SimOutriggers::getPosL()
+{
+  return posL;
+}
+
+double SimOutriggers::getPosR()
+{
+  return posR;
+}
