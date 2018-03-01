858851e603facc583e60776449bcad100f6d17cd
fworg64 <fworg64@gmail.com>
2018-02-28 20:31:36 -0600

    added basic outrigger actionlib server

--- src/controls/dig_control/src/outrigger_master.cpp
+++ src/controls/dig_control/src/outrigger_master.cpp
@@ -1,27 +1,77 @@
 #include<dig_control/OutriggerAction.h>
 #include<actionlib/server/simple_action_server.h>
+#include <outrigger_controller/outrigger_controller.h>
+#include <ros/ros.h>
+#include <vesc_access/vesc_access.h>
+#include <sim_robot/sim_outriggers.h>
 
+SimOutriggers *outriggerSimulation;
+iVescAccess *outriggerRightVesc;
+iVescAccess *outriggerLeftVesc;
+OutriggerController *outriggerC;
+bool simulating;
 
-void execute(const dig_control::outriggerGoalConstPtr & goal, 
-             actionlib::SimpleActionServer<dig_control::outriggerAction> * serv_ptr)
+void deployOutriggers(const dig_control::OutriggerGoalConstPtr & goal, 
+             actionlib::SimpleActionServer<dig_control::OutriggerAction> * serv_ptr)
 {
-
-
-  as->setSucceeded();
+  //deploy outriggers
+  outriggerC->deploy();
+  while (outriggerC->isDeployed() == false)
+  {
+    //make loop speed constant, can that be done with ros rate and sleep?
+    outriggerC->update(.02);
+    if (simulating)
+    {
+      outriggerSimulation->update(.02);
+    }
+  }
+  serv_ptr->setSucceeded();
 }
 
-
-
-
-
+void retractOutriggers(const dig_control::OutriggerGoalConstPtr & goal, 
+             actionlib::SimpleActionServer<dig_control::OutriggerAction> * serv_ptr)
+{
+  //deploy outriggers
+  outriggerC->retract();
+  while (outriggerC->isRetracted() == false)
+  {
+    //make loop speed constant, can that be done with ros rate and sleep?
+    outriggerC->update(.02);
+    if (simulating)
+    {
+      outriggerSimulation->update(.02);
+    }
+  }
+  serv_ptr->setSucceeded();
+}
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "outriggerAction.h");
   ros::NodeHandle n;
-  actionlib::SimpleActionServer<dig_control::outriggerAction> server(n, "outriggers_server", 
-                                       boost::bind(&execute, _1, &server), false);
-  server.start();
+
+  simulating = true; //TODO, read this from rosparam
+
+  actionlib::SimpleActionServer<dig_control::OutriggerAction> deployServer(n, "deploy_riggers", 
+                                       boost::bind(&deployOutriggers, _1, &deployServer), false);
+  actionlib::SimpleActionServer<dig_control::OutriggerAction> retractServer(n, "retract_riggers", 
+                                       boost::bind(&retractOutriggers, _1, &retractServer), false);
+
+  if (simulating)
+  {
+    outriggerSimulation = new SimOutriggers(0,0);
+    outriggerRightVesc = outriggerSimulation->getRVesc();
+    outriggerLeftVesc = outriggerSimulation->getLVesc();
+  }
+  else
+  {
+    outriggerSimulation = NULL;
+    //populate real VESCS here
+  }
+  outriggerC = new OutriggerController(outriggerLeftVesc, outriggerRightVesc);
+
+  deployServer.start();
+  retractServer.start();
   ros::spin();
   return 0;
 }
--- src/controls/dig_control/src/test/outrigger_tester.cpp
+++ src/controls/dig_control/src/test/outrigger_tester.cpp
@@ -1,18 +1,51 @@
 #include <dig_control/OutriggerAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <ros/ros.h>
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "outrigger_tester");
-  actionlib::SimpleActionClient<dig_control::outriggerAction> client("deploy_riggers", true); // true -> don't need ros::spin()
-  client.waitForServer();
-  dig_control::outriggersGoal goal;
-  // Fill in goal here
+  
+  actionlib::SimpleActionClient<dig_control::OutriggerAction> deployClient("deploy_riggers", true); // true -> don't need ros::spin()
+  deployClient.waitForServer();
+  
+  actionlib::SimpleActionClient<dig_control::OutriggerAction> retractClient("retract_riggers", true); // true -> don't need ros::spin()
+  retractClient.waitForServer();
 
-  client.sendGoal(goal);
-  client.waitForResult(ros::Duration(5.0));
-  if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    printf("Yay! The dishes are now clean");
-  printf("Current State: %s\n", client.getState().toString().c_str());
+  dig_control::OutriggerGoal goal;
+  // Fill in goal here
+  
+  ROS_INFO("SENDING GOAL");
+  deployClient.sendGoal(goal);
+  deployClient.waitForResult(ros::Duration(5.0));
+  ROS_INFO("DONE WAITING");
+  if (deployClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    printf("Yay! The outriggers are now deployed");
+  printf("Current State: %s\n", deployClient.getState().toString().c_str());
+
+  ROS_INFO("SENDING GOAL");
+  retractClient.sendGoal(goal);
+  retractClient.waitForResult(ros::Duration(5.0));
+  ROS_INFO("DONE WAITING");
+  if (retractClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    printf("Yay! The outriggers are now retracted");
+  printf("Current State: %s\n", retractClient.getState().toString().c_str());
+
+  ROS_INFO("SENDING GOAL");
+  deployClient.sendGoal(goal);
+  deployClient.waitForResult(ros::Duration(5.0));
+  ROS_INFO("DONE WAITING");
+  if (deployClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    printf("Yay! The outriggers are now deployed");
+  printf("Current State: %s\n", deployClient.getState().toString().c_str());
+
+  ROS_INFO("SENDING GOAL");
+  retractClient.sendGoal(goal);
+  retractClient.waitForResult(ros::Duration(5.0));
+  ROS_INFO("DONE WAITING");
+  if (retractClient.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    printf("Yay! The outriggers are now retracted");
+  printf("Current State: %s\n", retractClient.getState().toString().c_str());
+
   return 0;
 }
