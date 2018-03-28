66cae2dfb846188aa3bac2f769453c8064b4f094
Marc Olberding <dashneptune@gmail.com>
2018-03-28 14:57:17 -0500

    more changes to the lpf

--- src/navigation/mapping/include/mapping_filter/low_pass.h
+++ src/navigation/mapping/include/mapping_filter/low_pass.h
@@ -5,29 +5,25 @@
 #include <costmap_2d/layer.h>
 #include <costmap_2d/layered_costmap.h>
 #include <costmap_2d/GenericPluginConfig.h>
+#include <costmap_2d/obstacle_layer.h>
 #include <dynamic_reconfigure/server.h>
-
+#include <boost/circular_buffer.hpp>
 
 
 namespace low_pass_namespace
 {
-  class LowPassLayer : public costmap_2d::Layer, public costmap_2d::Costmap2D
+  class LowPassLayer : public costmap_2d::ObstacleLayer
   {
   public:
     explicit LowPassLayer ();
-    virtual void onInitialize ();
-  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
-                             double* max_y);
-  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
-  bool isDiscretized()
-  {
-    return true;
-  }
-
-  virtual void matchSize();
+    virtual void onInitialize () override;
+    virtual void updateCosts (costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j) override;
   private:
     void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
     dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv;
+    static constexpr unsigned int size_of_buffer = 10;
+    boost::circular_buffer<char *> buffman;
+
 
   };
 }
--- src/navigation/mapping/src/mapping_filter/low_pass.cpp
+++ src/navigation/mapping/src/mapping_filter/low_pass.cpp
@@ -10,69 +10,23 @@ using costmap_2d::NO_INFORMATION;
 
 namespace low_pass_namespace
 {
-  LowPassLayer::LowPassLayer ()
+  LowPassLayer::LowPassLayer () : buffman (this->size_of_buffer)
   {
 
   }
 
   void LowPassLayer::onInitialize ()
   {
-    ros::NodeHandle nh ("~/" + name_);
-    current_ = true;
-    default_value_ = NO_INFORMATION;
-    matchSize ();
-    dsrv = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
-    dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind (&LowPassLayer::reconfigureCB, this, _1, _2);
-    dsrv->setCallback(cb);
+    ObstacleLayer::onInitialize();
   }
 
-
-  void LowPassLayer::matchSize()
+  void LowPassLayer::updateCosts(costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i, int max_j)
   {
-    Costmap2D* master = layered_costmap_->getCostmap();
-    resizeMap(master->getSizeInCellsX(), master->getSizeInCellsY(), master->getResolution(),
-              master->getOriginX(), master->getOriginY());
-  }
-
-
-  void LowPassLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level)
-  {
-    enabled_ = config.enabled;
-  }
-
-  void LowPassLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
-                               double* min_y, double* max_x, double* max_y)
-  {
-    if (!enabled_)
-      return;
-
-    double mark_x = robot_x + cos(robot_yaw), mark_y = robot_y + sin(robot_yaw);
-    unsigned int mx;
-    unsigned int my;
-    if(worldToMap(mark_x, mark_y, mx, my)){
-      setCost(mx, my, LETHAL_OBSTACLE);
-    }
-
-    *min_x = std::min(*min_x, mark_x);
-    *min_y = std::min(*min_y, mark_y);
-    *max_x = std::max(*max_x, mark_x);
-    *max_y = std::max(*max_y, mark_y);
-  }
-
-  void LowPassLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
-                              int max_j)
-  {
-    if (!enabled_)
-      return;
-
-    for (int j = min_j; j < max_j; j++)
+    for (unsigned int i = min_i; i < max_i; i++)
     {
-      for (int i = min_i; i < max_i; i++)
+      for (unsigned int j = min_j; j <max_j; j++)
       {
-        int index = getIndex(i, j);
-        if (costmap_[index] == NO_INFORMATION)
-          continue;
-        master_grid.setCost(i, j, LETHAL_OBSTACLE);
+        
       }
     }
   }
