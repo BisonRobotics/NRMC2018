dbef77ab1c0b68fd94b36bc08bb4ac0222e84ae9
fworg64 <fworg64@gmail.com>
2018-03-28 22:27:21 -0500

    added constructor to backhoe to give limit switch support on the sim vescs it returns

--- src/controls/new_sim/src/sim_backhoe.cpp
+++ src/controls/new_sim/src/sim_backhoe.cpp
@@ -5,10 +5,19 @@ SimBackhoe::SimBackhoe(double shoulderTheta, double wristTheta)
   shTh = shoulderTheta;
   wrTh = wristTheta;
 
-  sh = new SimVesc(16, 0, 1.0, shoulderTheta, 0, 3.1);
-  wr = new SimVesc(16, 0, 1.0, wristTheta, 0, 3.1);
+  sh = new SimVesc(16, 0, 1.0);
+  wr = new SimVesc(16, 0, 1.0);
 }
 
+SimBackhoe::SimBackhoe(double shoulderTheta, double wristTheta, double shoulderBottomLimit,
+                       double shoulderUpperLimit, double wristBottomLimit, double wristUpperLimit)
+{
+  shTh = shoulderTheta;
+  wrTh = wristTheta;
+
+  sh = new SimVesc(16, 0, 1.0, shoulderTheta, shoulderBottomLimit, shoulderUpperLimit);
+  wr = new SimVesc(16, 0, 1.0, wristTheta, wristBottomLimit, wristUpperLimit);
+}
 void SimBackhoe::update(double dt)
 {
   sh->update(dt);
