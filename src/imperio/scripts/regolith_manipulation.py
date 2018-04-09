34adb368b3c735990a4376103dfdf8e75a17ba2f
Nicole Maguire <maguirne@dukes.jmu.edu>
2018-04-09 14:43:48 -0400

    needed to change branches real quick

--- src/imperio/scripts/regolith_manipulation.py
+++ src/imperio/scripts/regolith_manipulation.py
@@ -16,16 +16,28 @@ from dig_control.msg import DigAction, DigActionGoal
 class RegolithManipulation(object):
     def __init__(self):
         self.dig_client = actionlib.SimpleActionClient('dig_server', DigAction)
-        #self.dig_client.wait_for_server()
 
     # Tells the robot to dig the regolith
     # Returns boolean of success
     def dig_regolith(self, robot):
-        goal = DigActionGoal()
-        #TODO : fill in the goal here
-        self.dig_client.send_goal(goal)
+        print("enter dig regolith")
+        goal = self.dig_goal_message()
+        print("dig message created")
+        self.dig_client.wait_for_server()
+        print("finished waiting for the serer")
+        self.ac.send_goal(goal)
+        print("goal sent")
+        #TODO : Check how long we should do this
+        result = self.dig_client.get_goal_status_text()
+        print(result)
         #TODO : Wait for the result here?
         return True
+
+    def dig_goal_message(self):
+        goal = DigActionGoal
+        #TODO : fill in the angle
+        goal.angle = 0.0
+        return goal
 
     # Tells the robot to deposit the regolith
     # Returns boolean of success
