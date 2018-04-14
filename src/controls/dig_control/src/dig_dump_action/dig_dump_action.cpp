869ac0fbd62d138155dce91fda2f9868bfc8e780
fworg64 <fworg64@gmail.com>
2018-04-13 19:15:20 -0500

    Posting a goal to the dig server will now return a result successcfully!

--- src/controls/dig_control/src/dig_dump_action/dig_dump_action.cpp
+++ src/controls/dig_control/src/dig_dump_action/dig_dump_action.cpp
@@ -32,14 +32,14 @@ void DigDumpAction::digExecuteCB(const dig_control::DigGoalConstPtr &goal)
       r.sleep();
       switch (digging_state)
       {
-        case dig_state_enum::dig_idle: //not digging, should start here
+        case dig_state_enum::dig_idle: //state 0//not digging, should start here
           bucket->turnSifterOn();
           bucket->turnLittleConveyorOn();
           backhoe->setShoulderSetpoint(.7); //put system in known starting config
-          backhoe->setWristSetpoint(.13);
+          backhoe->setWristSetpoint(.02);
           digging_state = ensure_at_measurement_start;
         break;
-        case dig_state_enum::ensure_at_measurement_start: 
+        case dig_state_enum::ensure_at_measurement_start: //state 1
           if (backhoe->shoulderAtSetpoint() && backhoe->wristAtSetpoint())
           {
             backhoe->setShoulderSetpoint(.01); //take it to close to ground
@@ -47,7 +47,7 @@ void DigDumpAction::digExecuteCB(const dig_control::DigGoalConstPtr &goal)
             weightMetric =0;
           }
           break;
-        case dig_state_enum::moving_to_ground:
+        case dig_state_enum::moving_to_ground: //state 2
           //integrate work moving from measurement start to where we think the ground is
           weightMetric += backhoe->getShoulderTorque() * backhoe->getShoulderVelocity() * .02; //.02 is dt
           if (backhoe->shoulderAtSetpoint())
@@ -57,7 +57,7 @@ void DigDumpAction::digExecuteCB(const dig_control::DigGoalConstPtr &goal)
               dig_result.weight_harvested = weightMetric;
           }
           break;
-        case dig_state_enum::finding_ground: //going to find the ground
+        case dig_state_enum::finding_ground: //state 3 //going to find the ground
           //this method checks the torque reported by the motor
           //it waits for the I gain on the velocity control to increase the torque above
           //a threshold
@@ -68,28 +68,28 @@ void DigDumpAction::digExecuteCB(const dig_control::DigGoalConstPtr &goal)
               digging_state = curling_backhoe;
           }
           break;
-        case dig_state_enum::curling_backhoe: //curling wrist into dirt
+        case dig_state_enum::curling_backhoe: //state 4 //curling wrist into dirt
             if (backhoe->wristAtSetpoint())
             {
-                backhoe->setShoulderSetpoint(-.7); //lift to angle appropiate for dropping dirt into bucket
+                backhoe->setShoulderSetpoint(.7); //lift to angle appropiate for dropping dirt into bucket
                 digging_state = moving_arm_to_initial;
             }
           break;
-        case dig_state_enum::moving_arm_to_initial: //lifting dug dirt up
+        case dig_state_enum::moving_arm_to_initial: //state 5 //lifting dug dirt up
             if (backhoe->shoulderAtSetpoint())
             {
               backhoe->setWristSetpoint(0); //curl it out
               digging_state = dumping_into_bucket;
             }
           break;
-        case dig_state_enum::dumping_into_bucket: //uncurling wrist to release dirt into bucket
+        case dig_state_enum::dumping_into_bucket: //state 6 //uncurling wrist to release dirt into bucket
             if (backhoe->wristAtSetpoint())
             {
-                backhoe->setShoulderSetpoint(-1.1); //wherever transit/initial should be
+                backhoe->setShoulderSetpoint(1.1); //wherever transit/initial should be
                 digging_state = returning_backhoe_to_initial;
             }
           break;
-        case dig_state_enum::returning_backhoe_to_initial: //moving back to same position as dig idle
+        case dig_state_enum::returning_backhoe_to_initial: //state 7 //moving back to same position as dig idle
             if (backhoe->shoulderAtSetpoint())
             {
                 bucket->turnLittleConveyorOff();
