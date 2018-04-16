f1e4a07267fe6fe04ecf69d11a2684261025984a
Nicole Maguire <maguirne@dukes.jmu.edu>
2018-04-16 14:37:36 -0400

    Added the dumping actionlib into the regolith manipulation file

--- src/imperio/scripts/regolith_manipulation.py
+++ src/imperio/scripts/regolith_manipulation.py
@@ -12,19 +12,26 @@ import roslib
 import actionlib
 
 from dig_control.msg import DigAction, DigActionGoal, DigActionResult
+from dig_control.msg import DumpAction, DumpActionGoal, DumpActionResult
+
 
 class RegolithManipulation(object):
     def __init__(self):
         self.dig_client = actionlib.SimpleActionClient('dig_server', DigAction)
+        self.dump_client = actionlib.SimpleActionClient('dump_server', DumpAction)
         self.regolith_in_bucket = 0
         self.waiting_on_action = False
+        self.halt = False
 
     # Tells the robot to dig the regolith
     # Returns boolean of success
     def dig_regolith(self, robot):
+        if self.halt:
+            return None
+
         if self.waiting_on_action == True:
             result = self.dig_client.get_result()
-            if result == None:
+            if result == None: #The action is not yet complete
                 return False
             else:
                 if result.is_error:
@@ -49,17 +56,43 @@ class RegolithManipulation(object):
         self.dig_client.send_goal(goal)
 
     def dig_goal_message(self):
-        goal = DigActionGoal
-        #TODO : fill in the angle [JIRA NRMC2018-358]
-        goal.angle = 10.0
+        goal = DigActionGoal()
         return goal
 
     # Tells the robot to deposit the regolith
     # Returns boolean of success
     def deposit_regolith(self, robot):
-        #deposit regolith
-        return True
+        if self.halt:
+            return None
+
+        if self.waiting_on_action == True:
+            result = self.dump_client.get_result()
+            if result == None: #The action is not yet complete
+                return False
+            else:
+                if result.is_error:
+                    return None
+                self.waiting_on_action = False
+                self.regolith_in_bucket = result.weight_in_bucket
+                print("Imperio : Bucket now {} full".format(self.regolith_in_bucket))
+
+
+        accepted_empty_threshold = 0 #TODO: Confirm this threshold with them [JIRA NRMC2018-358]
+        if self.regolith_in_bucket > accepted_empty_threshold:
+            self.single_dig()
+            self.waiting_on_action = True
+        return False
+
+    def single_dump(self):
+        print("Imperio : Performing a dump")
+        goal = self.dump_goal_message()
+        self.dig_client.wait_for_server()
+        self.dig_client.send_goal(goal)
+
+    def dump_goal_message(self):
+        goal = DumpActionGoal()
+        return goal
 
     # The commands to tell the robot to stop moving the hoe
-    def halt_regolithm_commands(self, robot):
-        pass
+    def halt_regolith_commands(self, robot):
+        self.halt = True
