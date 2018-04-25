ca72e61adff4a4b7914b9183e9e84ddec2e08408
Marc Olberding <dashneptune@gmail.com>
2018-04-24 22:09:20 -0500

    updated the small and large conveyor drive types and values

--- src/controls/dig_control/src/bucket_controller/bucket_controller.cpp
+++ src/controls/dig_control/src/bucket_controller/bucket_controller.cpp
@@ -13,7 +13,7 @@ BucketController::BucketController(iVescAccess *bigConveyorVesc, iVescAccess *li
 
 void BucketController::turnBigConveyorOn()
 {
-  bc->setDuty(.5);
+  bc->setLinearVelocity(7000); // in erpm
   big_conveyor_state = true;
 }
 
@@ -25,7 +25,7 @@ void BucketController::turnBigConveyorOff()
 
 void BucketController::turnLittleConveyorOn()
 {
-  lc->setDuty(.9);
+  lc->setTorque(7); // in Amps
   little_conveyor_state = true;
 }
 
