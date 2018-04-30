1b11c6820463410118159148085d70f85238ec78
Marc Olberding <dashneptune@gmail.com>
2018-04-30 16:31:12 -0500

    fixed the fucking costmap order

--- src/navigation/mapping/src/mapping_filter/low_pass.cpp
+++ src/navigation/mapping/src/mapping_filter/low_pass.cpp
@@ -19,7 +19,7 @@ void LowPassLayer::onInitialize()
   ros::NodeHandle gl_nh;
   current_ = true;
   default_value_ = NO_INFORMATION;  // we can change this later if we want
-  enabled_ = false;
+  enabled_ = true;
   enable_service = gl_nh.advertiseService ("enable_mapping", &LowPassLayer::updateEnable, this);
 }
 
@@ -30,18 +30,23 @@ bool LowPassLayer::updateEnable (std_srvs::SetBool::Request &req, std_srvs::SetB
   return true;
 }
 
+void LowPassLayer::updateBounds (double robot_x, double robot_y, double robot_yaw, double *min_x, double *min_y, double *max_x, double *max_y)
+{
+  ObstacleLayer::updateBounds (robot_x, robot_y, robot_yaw, min_x, min_y, max_x, max_y);
+}
+
+
 void LowPassLayer::updateCosts(costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i, int max_j)
 {
   if (enabled_)
   {
     unsigned char *my_map = master_grid.getCharMap();
     unsigned int size_of_map = master_grid.getSizeInCellsX() * master_grid.getSizeInCellsY();
-    cv::Mat input = cv::Mat(master_grid.getSizeInCellsX(), master_grid.getSizeInCellsY(), CV_8U, my_map,
-                            cv::Mat::AUTO_STEP);
+    cv::Mat input = cv::Mat(master_grid.getSizeInCellsX(), master_grid.getSizeInCellsY(), CV_8U);
     cv::Mat filtered;
-
-    cv::medianBlur(input, filtered, size_of_kern);
-
+    std::memcpy (input.data, my_map,sizeof(unsigned char) * size_of_map);
+    //std::memset (my_map, LETHAL_OBSTACLE, sizeof(unsigned char) * size_of_map);
+    cv::medianBlur(input, filtered, 129);
     std::memcpy(my_map, filtered.data, sizeof(unsigned char) * size_of_map);
     ObstacleLayer::updateCosts(master_grid, min_i, min_j, max_i, max_j);
   }
