813ead3a46d4c0dd86d3ed442178113f72c35df8
fworg64 <fworg64@gmail.com>
2018-05-03 23:32:38 -0500

    changed parameters v2

--- src/controls/dig_control/src/dig_dump_action/dig_dump_action.cpp
+++ src/controls/dig_control/src/dig_dump_action/dig_dump_action.cpp
@@ -1,14 +1,14 @@
 
 #include "dig_dump_action/dig_dump_action.h"
 
-#define LINEAR_RETRACTED_POINT .05
-#define LINEAR_EXTENDED_POINT .14
+#define LINEAR_RETRACTED_POINT .047
+#define LINEAR_EXTENDED_POINT .155
 #define CENTRAL_MEASUREMENT_START_ANGLE 2.0
 #define CENTRAL_MEASUREMENT_STOP_ANGLE 1.5
 #define CENTRAL_HOLD_TORQUE -1          //increase the magintude
 #define CENTRAL_TRANSPORT_ANGLE 2.1                 // move this up
-#define CENTRAL_MOVE_ROCKS_INTO_HOPPER_ANGLE  2.8 // move this up
-#define CENTRAL_DUMP_ANGLE 2.0        // must be below safety point, where backhoe dumps into bucket
+#define CENTRAL_MOVE_ROCKS_INTO_HOPPER_ANGLE  2.65 // move this up
+#define CENTRAL_DUMP_ANGLE 2.15        // must be below safety point, where backhoe dumps into bucket
 #define CENTRAL_DEPOSITION_ANGLE 2.9  // must be below max position
 
 #define GROUND_ALPHA .2
