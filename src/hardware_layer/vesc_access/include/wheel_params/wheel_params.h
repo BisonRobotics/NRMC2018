f0545750f741d66f6851e447c2768d8c18a786db
fworg64 <fworg64@gmail.com>
2018-05-03 23:35:51 -0500

    changed parameters v2

--- src/hardware_layer/vesc_access/include/wheel_params/wheel_params.h
+++ src/hardware_layer/vesc_access/include/wheel_params/wheel_params.h
@@ -81,7 +81,7 @@
 #define LINEAR_ACTUATOR_LENGTH .1778
 #define MINIMUM_CENTRAL_ANGLE 0
 #define MAXIMUM_CENTRAL_ANGLE 2.96
-#define SAFE_CENTRAL_ANGLE 2.2
+#define SAFE_CENTRAL_ANGLE 2.3
 #define SAFE_LINEAR_DISTANCE .04985
 
 nsVescAccess::vesc_param_struct_t front_left_param = {.max_velocity = MAX_WHEEL_VELOCITY,
