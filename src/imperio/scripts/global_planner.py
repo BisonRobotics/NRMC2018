a125581026ed60d5a664186eb902bd356ff94048
Nicole Maguire <maguirne@dukes.jmu.edu>
2018-05-09 22:13:55 -0400

    User warnings

--- src/imperio/scripts/global_planner.py
+++ src/imperio/scripts/global_planner.py
@@ -34,11 +34,15 @@ class GlobalPlanner(Planner):
         #TODO [JIRA : NRMC2018-577] might run just one non threaded path to check if there IS a possible path in the expanded map
         #TODO [JIRA : NRMC2018-578] possible run the two results against each other in case minimal path is better
         results = RRT.find_best_rrt_path(location, goal, self.expanded_map, 20)
+
         if len(results) == 0:
             rospy.logwarn("[IMPERIO] : NO POSSIBLE PATH FOUND FOR EXPANDED MAP")
             rospy.loginfo("[IMPERIO] : Switching to the minimal map")
             results = RRT.find_best_rrt_path(location, goal, self.minimal_map, 20)
+            if len(results) == 0:
+                rospy.logwarn("[IMPERIO] : CRITICAL FAILURE")
+                rospy.logwarn("[IMPERIO] : NO POSSIBLE PATH FOUND FOR MINIMAL MAP")
 
         rospy.loginfo("[IMPERIO] : Path Planning Complete. Total path planning time: {} seconds".format(time.time() - saved_time))
         return results
-    
+
