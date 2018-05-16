48ac0e909437b36e220d5db2257fc2d6842e94b2
Marc Olberding <dashneptune@gmail.com>
2018-05-15 21:37:31 -0500

    added in the dig teleop stuff

--- src/controls/dig_control/src/dig_teleop_by_hand.cpp
+++ src/controls/dig_control/src/dig_teleop_by_hand.cpp
@@ -9,39 +9,45 @@
 
 BackhoeController *global_backhoe;
 BucketController *global_bucket;
-OutriggerController *global_outrigger;
 
 void callback(const sensor_msgs::Joy::ConstPtr &joy)
 {
-  float linear = 0.0f;
-  float central = 0.0f;
+  bool linear_up = false;
+  bool linear_down = false;
+  bool central_up = false;
+  bool central_down = false;
   bool sifter_toggle = false;
-  bool large_conveyor_toggle = false;
   static constexpr float linear_gain = 6.0;
   static constexpr float central_gain = 8.0f;
 
-  if (joy->buttons[4])  // left bumper is safety
+  if (joy->buttons[5])  // right bumper is safety
   {
-    linear = joy->axes[1];                         // left analog up/down
-    central = joy->axes[4];                        // right analog up/down
-    sifter_toggle = joy->buttons[0] != 0;          // A button
-    large_conveyor_toggle = joy->buttons[1] != 0;  // B button
-
-    if (joy->buttons[2] && !joy->buttons[3])      /* should we toggle */
-    {
-      global_outrigger->deploy();
-      ROS_INFO("DEPLOY");
-
-    }
-    else if (joy->buttons[3] && !joy->buttons[2])
-    {
-      global_outrigger->retract();
-    }
+    linear_up = joy->buttons[3];                  // Y is linear up
+    linear_down = joy->buttons[2];                // X is linear down
+    central_up = joy->buttons[1];                 // B is central up
+    central_down = joy->buttons[0];               // A is central down
+    sifter_toggle = joy->axes[2] == 1.0f;          //LT is sifter stuff
   }
 
-  if (fabs(central) > .001 )
+  if (linear_up && linear_down)
   {
-    global_backhoe->setShoulderTorque(central_gain * central);
+    linear_up = false;
+    linear_down = false;
+  }
+
+  if (central_up && central_down)
+  {
+    central_up = false;
+    central_down = false;
+  }
+
+  if (central_up)
+  {
+    global_backhoe->setShoulderTorque(central_gain);
+  }
+  else if (central_down)
+  {
+    global_backhoe->setShoulderTorque(-central_gain);
   }
   else
   {
@@ -49,9 +55,13 @@ void callback(const sensor_msgs::Joy::ConstPtr &joy)
     global_backhoe->stopShoulder();
   }
 
-  if (fabs(linear) > .001)
+  if (linear_up)
   {
-    global_backhoe->setWristTorque(linear_gain * linear);
+    global_backhoe->setWristTorque(linear_gain);
+  }
+  else if (linear_down)
+  {
+    global_backhoe->setWristTorque(-linear_gain);
   }
   else
   {
@@ -59,15 +69,9 @@ void callback(const sensor_msgs::Joy::ConstPtr &joy)
     global_backhoe->stopWrist();
   }
 
-
-
-  if (sifter_toggle)
-  {
+  if (sifter_toggle) {
     global_bucket->toggleSifter();
     global_bucket->toggleLittleConveyor();
-  }
-  if (large_conveyor_toggle)
-  {
     global_bucket->toggleBigConveyor();
   }
 }
@@ -93,12 +97,10 @@ int main(int argc, char **argv)
 
   BucketController bucket(&large_conveyor_vesc, &small_conveyor_vesc, &sifter_vesc);
 
-  OutriggerController outrigger(&left_outrigger_vesc, &right_outrigger_vesc);
   global_backhoe = &backhoe;
   global_bucket = &bucket;
-  global_outrigger = &outrigger;
 
-  ros::Subscriber joy_sub = n.subscribe("dig_joy", 10, callback);
+  ros::Subscriber joy_sub = n.subscribe("joy", 10, callback);
 
   ros::Rate r(40);
   ros::Time initial = ros::Time::now();
@@ -119,7 +121,6 @@ int main(int argc, char **argv)
     r.sleep();
     ros::spinOnce();
     backhoe.update((initial - ros::Time::now()).toSec());
-    outrigger.update((initial - ros::Time::now()).toSec());
     ROS_INFO ("linear position: %.4f central position: %.4f ",linearSafety.getPositionEstimate(),backhoeSafety.getPositionEstimate() );
     initial = ros::Time::now();
     velocity = small_conveyor_vesc.getTorque();
